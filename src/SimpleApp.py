a2dd03e061ff5dded47170a8ef6983bc8818a65e
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-03-11 22:19:11 +0100

    fix test env

--- src/SimpleApp.py
+++ src/SimpleApp.py
@@ -1,15 +1,9 @@
 """SimpleApp.py"""
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master("spark-worker:7077").appName("Word Count").config("spark.some.config.option", "some-value").getOrCreate()
-
-logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
-spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
-logData = spark.read.text(logFile).cache()
-
-numAs = logData.filter(logData.value.contains('a')).count()
-numBs = logData.filter(logData.value.contains('b')).count()
-
-print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+spark = SparkSession.builder \
+    .master('spark://127.0.0.1:7077') \
+    .appName("SimpleApp") \
+    .getOrCreate()
 
 spark.stop()
