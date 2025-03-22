7adb7a5daee7337764f39aed230f1b727fd4fe34
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-03-22 14:57:54 +0100

    removed etl-script

--- src-etl-microservice/src/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from controllers.etlcontroller import etl_blueprint
-
-app = Flask(__name__)
-app.register_blueprint(etl_blueprint)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
