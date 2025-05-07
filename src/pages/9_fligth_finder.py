f7cd01eb36d8ae65a81c135326dc4083af031d3f
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-05-07 19:11:42 +0200

    add redis [skip ci]

--- src/pages/9_fligth_finder.py
+++ src/pages/9_fligth_finder.py
@@ -1,4 +1,4 @@
-from datetime import datetime, date, time
+from datetime import date, time
 
 import airportsdata
 import pandas as pd
---
+++ src/utils/session_redis.py
@@ -0,0 +1,26 @@
+
+
+import os
+import redis
+import json
+
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_db: int = int(os.getenv('REDIS_DB', 0))
+
+r = redis.Redis(host=redis_host, port=6379, db=redis_db)
+
+def get_client():
+    return r
+
+async def get_from_cache(key):
+    value = await r.get(key)
+    if value is not None:
+        return json.loads(value)
+    return None
+
+def save_to_cache(key, value, expiration=None):
+    serialized_value = json.dumps(value)
+    if expiration:
+        r.setex(key, expiration, serialized_value)
+    else:
+        r.set(key, serialized_value)
