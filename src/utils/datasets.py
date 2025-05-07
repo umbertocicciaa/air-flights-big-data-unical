e3f0b16e0c1ac784402483ace07f1ec68326b179
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-05-07 19:21:57 +0200

    add spark caching

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -4,4 +4,5 @@ from utils.session_spark import create_session
 def read_parquet(parquet_path):
     spark = create_session()
     data = spark.read.parquet(parquet_path)
+    data.cache()
     return data
